6c2d4452011b00d4ee936aa355aa7916fdedf113
pbnick <pbnick@gmx.ch>
2018-11-19 16:09:15 +0100

    added edit field and button to translate text to morse

--- app/src/main/java/com/example/taamefl2/appquest_morse/MainActivity.java
+++ app/src/main/java/com/example/taamefl2/appquest_morse/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.taamefl2.appquest_morse;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.json.JSONArray;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MorseEncoder morseEncoder = new MorseEncoder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText textForMorsing = findViewById(R.id.addText);
+
+        final Button textToMorse = findViewById(R.id.textToMorse);
+        textToMorse.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                try {
+                    morseEncoder.textToCode(textForMorsing.getText().toString());
+                    //TODO: add logic to send morse to display and do magic stuff of dooooom
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                textForMorsing.setText("");
+            }
+        });
     }
 }

a380b2f64b263ad367b9a3f9af57503b4b3063d1
TAAMEFL2 <florence.meier@swisscom.com>
2018-11-29 21:53:59 +0100

    Morsen geht so einigermassen

--- app/src/main/java/com/example/taamefl2/appquest_morse/MainActivity.java
+++ app/src/main/java/com/example/taamefl2/appquest_morse/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.taamefl2.appquest_morse;
 
+import android.graphics.Color;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-
-import org.json.JSONArray;
+import android.widget.TextView;
 
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,28 +30,41 @@ public class MainActivity extends AppCompatActivity {
         textToMorse.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 try {
-                    List<Primitive> morseCode = morseEncoder.textToCode(textForMorsing.getText().toString());
-                    for (Primitive bit: morseCode){
-                        switch (bit.getTextRepresentation()){
-                            case "·":
-                                Log.i("DIT", "Code");
-                                break;
-                            case "−":
-                                Log.i("DAH", "Code");
-                                break;
-                            case " ":
-                                Log.i("GAP", "Code");
-                                break;
-                            case "   ":
-                                Log.i("SYMBOLGAP", "Code");
-                                break;
-                            case " / ":
-                                Log.i("WORDGAP", "Code");
-                                break;
-                            default:
-                                Log.i("DEFAULT", "Code");
-                                break;
-                        }
+                    // Encode Text to Morse
+                    final List<Primitive> morseCode = morseEncoder.textToCode(textForMorsing.getText().toString());
+
+                    // Get textinput and button out of the way:
+                    ((ViewGroup)textForMorsing.getParent()).removeView(textForMorsing);
+                    ((ViewGroup)textToMorse.getParent()).removeView(textToMorse);
+
+                    // Flash screen (TODO: Check Timing, Error Handling, Commenting, Make functions and not line of doooom)
+                    final Handler handler = new Handler();
+                    for (int a = 0; a < morseCode.size() ;a++) {
+                        final int finalA = a;
+                        handler.postDelayed(new Runnable() {
+                            public void run() {
+                                switch (morseCode.get(finalA).getTextRepresentation()){
+                                    case "·":
+                                        getWindow().getDecorView().setBackgroundColor(Color.WHITE);
+                                        break;
+                                    case "−":
+                                        getWindow().getDecorView().setBackgroundColor(Color.WHITE);
+                                        break;
+                                    case " ":
+                                        getWindow().getDecorView().setBackgroundColor(Color.BLACK);
+                                        break;
+                                    case "   ":
+                                        getWindow().getDecorView().setBackgroundColor(Color.BLACK);
+                                        break;
+                                    case " / ":
+                                        getWindow().getDecorView().setBackgroundColor(Color.BLACK);
+                                        break;
+                                    default:
+                                        getWindow().getDecorView().setBackgroundColor(Color.RED);
+                                        break;
+                                }
+                            }
+                            }, morseCode.get(finalA).getSignalLengthInDits() * 500 * finalA);
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
